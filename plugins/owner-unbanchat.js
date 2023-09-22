49e87f9fdb9349e9d9607ad38faa1704995c6947
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-22 14:22:50 -0600

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝚃𝙴 𝙲𝙷𝙰𝚃 𝙵𝚄𝙴 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾,Ya estoy disponible🌟*');
+  m.reply('[➡️] 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐨, 𝐜𝐡𝐚𝐭 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐝𝐨 𝐞𝐱𝐢𝐭𝐨𝐬𝐚𝐦𝐞𝐧𝐭𝐞.');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
