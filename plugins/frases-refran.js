f225956316a821c997769e4042954c9bc789d799
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-24 08:36:06 -0500

    Update frases-refran.js

--- plugins/frases-refran.js
+++ plugins/frases-refran.js
@@ -7,7 +7,7 @@ var handler = async (m, { conn, text}) => {
 let hasil = Math.floor(Math.random() * 5000)
 
 conn.reply(m.chat, `*┏━☯︎☯︎☯︎☘︎☯︎☯︎☯︎☘︎☯︎☯︎☯︎━┓*
-*_${pickRandom(global.refran2)}_*\n┗━☯︎☯︎☯︎☘︎☯︎☯︎☯︎☘︎☯︎☯︎☯︎━┛*`, m, { contextInfo:{ externalAdReply: {title: '𝐑𝐄𝐅𝐑𝐀𝐍', body: '𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃-𝙼𝙳', sourceUrl: global.md, thumbnail: imagen7}}})
+\n*➪ _*${pickRandom(global.refran2)}*_ *\n\n┗━☯︎☯︎☯︎☘︎☯︎☯︎☯︎☘︎☯︎☯︎☯︎━┛*`, m, { contextInfo:{ externalAdReply: {title: '𝐑𝐄𝐅𝐑𝐀𝐍', body: '𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃-𝙼𝙳', sourceUrl: global.md, thumbnail: imagen7}}})
 
 }
 handler.help = ['refran']
