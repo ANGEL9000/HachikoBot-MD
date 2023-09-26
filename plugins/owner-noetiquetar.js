e9301d3f45d3cfae8e0aa6b948d2fcb9a1a9538c
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-26 18:13:12 -0500

    Update owner-noetiquetar.js

--- plugins/owner-noetiquetar.js
+++ plugins/owner-noetiquetar.js
@@ -1,9 +1,11 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
- m.reply(`*[ ⚠ ️] No etiquetes a mi creador, si es algo urgente contacta con el a su chat privado*`)
+conn.reply(m.chat, `*[ ⚠ ️] No etiquetes a mi creador, si es algo urgente contacta con el a su chat privado*` ,m, { contextInfo:{ externalAdReply: {title: '𝐍𝐎-𝐄𝐓𝐈𝐐𝐔𝐄𝐓𝐀𝐑', body: 'ᴇsᴄʀɪʙɪʀʟᴇ ᴀᴘʟᴀsᴛᴀɴᴛᴏ ᴀǫᴜɪ', sourceUrl: 'https://wa.me/51992004117', thumbnail: imagen7}}})
 }
 handler.customPrefix = /@51992004117/i;
 handler.command = new RegExp();
 
 export default handler;
+
+//conn.reply(m.chat, `hhh`, m, { contextInfo:{ externalAdReply: {title: 'titulo', body: 'cuero', sourceUrl: global.md, thumbnail: imagen7}}})
