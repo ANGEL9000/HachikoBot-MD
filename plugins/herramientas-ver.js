f8cbab47a249c76e84719d40fbc340f21bf80d7d
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-02 21:54:48 -0500

    Update herramientas-ver.js

--- plugins/herramientas-ver.js
+++ plugins/herramientas-ver.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn }) => {
 
-if (!/viewOnce/.test(m.quoted?.mtype)) throw '*📍 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾 𝙾 𝚅𝙸𝙳𝙴𝙾 𝚀𝚄𝙴 𝚃𝙸𝙴𝙽𝙴 𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝚄𝙽𝙰 𝚅𝙴𝚉 *'
+if (!/viewOnce/.test(m.quoted?.mtype)) throw '*📍 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽𝙰 𝙵𝙾𝚃𝙾 𝙾 𝚅𝙸𝙳𝙴𝙾 𝚀𝚄𝙴 𝚃𝙸𝙴𝙽𝙴 𝙿𝙰𝚁𝙰 𝚅𝙴𝚁 𝚄𝙽𝙰 𝚅𝙴𝚉*'
 let mtype = Object.keys(m.quoted.message)[0]
 let buffer = await m.quoted.download()
 let caption = m.quoted.message[mtype].caption || ''
