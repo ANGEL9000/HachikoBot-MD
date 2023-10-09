87434284b9a1dacdb2f5b254e153c571a5296ee9
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-08 19:36:00 -0500

    Update owner-ofuscar.js

--- plugins/owner-ofuscar.js
+++ plugins/owner-ofuscar.js
@@ -1,7 +1,7 @@
 import JavaScriptObfuscator from 'javascript-obfuscator'
 
 var handler = async(m, { conn, text }) => {
-if (!text) return m.reply(`*⚠️ QUE CÓDIGO VAS A OFUSCAR?*`) 
+if (!text) return m.reply(`*⚠️ 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 𝙲𝙾𝙳𝙸𝙶𝙾 𝚀𝚄𝙴 𝚅𝙰𝚂 𝙰 𝙾𝙵𝚄𝚂𝙲𝙰𝚁 ?*`) 
 function obfuscateCode(code) {
   return JavaScriptObfuscator.obfuscate(code, { compact: false, controlFlowFlattening: true, deadCodeInjection: true, simplify: true, numbersToExpressions: true }).getObfuscatedCode();
 }
