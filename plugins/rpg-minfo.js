eb24da0f0460d5579ba659276aa33af7e0c6a8f4
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-14 09:25:43 -0500

    Update rpg-minfo.js

--- plugins/rpg-minfo.js
+++ plugins/rpg-minfo.js
@@ -1,2 +1,4 @@
 import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, participants, isPrems}) => {
+const {money, joincount} = global.db.data.users[m.sender];
+const {exp, limit, level, role} = global.db.data.users[m.sender];
