e113cc0795b0f40ff4e1175cf5579166a585c9bc
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-15 13:26:58 -0500

    Update marker-lesbiana.js

--- plugins/marker-lesbiana.js
+++ plugins/marker-lesbiana.js
@@ -1,9 +1,9 @@
  const handler = async (m, {conn}) => {
   const vn = './media/gay2.mp3';
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lesbiana', {
+  await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lesbian', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙿𝚁𝙴𝚂𝙾*', m);
+  }), 'error.png', '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙰 𝙻𝙴𝚂𝙱𝙸𝙰𝙽𝙰*', m);
   //await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 handler.help = ['gay'];
