f1c6e93fe639ae606c81c3cd381666faba0eef2e
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-15 13:11:58 -0500

    Update maker-jail.js

--- plugins/maker-jail.js
+++ plugins/maker-jail.js
@@ -4,7 +4,7 @@
   await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/jail', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
   }), 'error.png', '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙿𝚁𝙴𝚂𝙾*', m);
-  await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+  //await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 handler.help = ['gay'];
 handler.tags = ['maker'];
