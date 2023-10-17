6ce10f75a661ef1c2a3b81803905db5738b42f2d
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-16 21:22:53 -0500

    Update maker-jail.js

--- plugins/maker-jail.js
+++ plugins/maker-jail.js
@@ -3,7 +3,7 @@
   const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   await conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/jail', {
     avatar: await conn.profilePictureUrl(who, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'error.png', '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙿𝚁𝙴𝚂𝙾*', m);
+  }), 'error.png', '*𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙿𝚁𝙴𝚂𝙾*', m, { contextInfo:{ externalAdReply: {title: wm, body: '𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃-𝙼𝙳', sourceUrl: global.md, thumbnail: imagen7 }}});
   //await conn.sendMessage(m.chat, {audio: {url: vn}, fileName: `error.mp3`, mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 handler.help = ['gay'];
